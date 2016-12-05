7d1fb91fa6a2a5c95160d9ced2d6ce5fcfdb5dca
Denys Levchenko <denis.levtchenko@gmail.com>
2016-12-06 00:09:51 +0200

    day1: group patterns

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,43 +5,27 @@ use std::path::Path;
 use std::collections::HashSet;
 
 
-fn find_location(steps: &Vec<Vec<&str>>) -> (i16, i16, i16, i16) {
+fn find_location(steps: &Vec<Vec<&str>>) -> (i16, i16) {
     let mut x: i16 = 0;
     let mut y: i16 = 0;
     let mut face = "N";
-    let mut seen: HashSet<(i16, i16)> = HashSet::new();
-    let mut seen_twice: (i16, i16) = (0, 0);
-    for (index, step) in steps.iter().enumerate() {
-        {
-            let direction = match index % 2 {
-                0 => &mut x,
-                _ => &mut y,
-            };
-            let step_int = step[1].parse().unwrap();
-            match (face, step[0]) {
-                ("N", "R") => {*direction += step_int; face = "W"},
-                ("N", "L") => {*direction -= step_int; face = "E"},
+    //let mut seen: HashSet<(i16, i16)> = HashSet::new();
+    //let mut seen_twice: (i16, i16) = (0, 0);
+    for step in steps {
+        let step_int = step[1].parse().unwrap();
+        match (face, step[0]) {
+            ("S", "L") | ("N", "R") => {x += step_int; face = "W"},
 
-                ("W", "L") => {*direction += step_int; face = "N"},
-                ("W", "R") => {*direction -= step_int; face = "S"},
+            ("W", "L") | ("E", "R") => {y += step_int; face = "N"},
 
-                ("S", "L") => {*direction += step_int; face = "W"},
-                ("S", "R") => {*direction -= step_int; face = "E"},
+            ("S", "R") | ("N", "L") => {x -= step_int; face = "E"},
 
-                ("E", "R") => {*direction += step_int; face = "N"},
-                ("E", "L") => {*direction -= step_int; face = "S"},
-                _ => {},
-            };
-        }
-        if seen_twice == (0, 0) && seen.contains(&(x, y)) {
-            seen_twice = (x, y);
-            println!("{} {}", x, y);
-        } else {
-            seen.insert((x, y));
-        }
+            ("E", "L") | ("W", "R") => {y -= step_int; face = "S"},
+            _ => {},
+        };
 
     };
-    (x.abs(), y.abs(), seen_twice.0.abs(), seen_twice.1.abs())
+    (x.abs(), y.abs())
 
 }
 
@@ -54,8 +38,7 @@ fn main() {
     let steps: Vec<Vec<&str>> = s.split(',').map(|r| {
         r.trim().splitn(3, "").filter(|&s| s != "").collect()
     }).collect();
-    let (x, y, x1, y1) = find_location(&steps);
+    let (x, y) = find_location(&steps);
     println!("Answer 1: {} + {} = {}", x, y, x + y);
-    println!("Answer 2: {} + {} = {}", x1, y1, x1 + y1);
 
 }
